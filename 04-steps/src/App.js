916714f84f743ab7ad9811066bd200f9317d8ccb
Victor Kovalchuk <vict02r05@gmail.com>
2026-02-16 22:49:59 +0200

    practice with children props

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -42,21 +42,15 @@ function Steps() {
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
 
-          <p className="message">Step: {step}: {messages[step - 1]}</p>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+          </StepMessage>
 
           <div className="buttons">
-            <Button 
-              textColor='#fff' 
-              bgColor='#7950f2' 
-              onClick={handlePrevious}
-            >
+            <Button onClick={handlePrevious}>
               <span>👈</span> Previous
             </Button>
-            <Button 
-              textColor='#fff' 
-              bgColor='#7950f2' 
-              onClick={handleNext}
-            >
+            <Button onClick={handleNext}>
               Next <span>👉</span>
             </Button>
           </div>
@@ -66,10 +60,19 @@ function Steps() {
   );
 }
 
-function Button({ textColor, bgColor, onClick, children }) {
+function StepMessage({ step, children }) {
+  return (
+    <p className="message">
+      <h3>Step: {step}</h3>
+      {children}
+    </p>
+  );
+}
+
+function Button({ onClick, children }) {
   return (
     <button
-      style={{backgroundColor: bgColor, color: textColor}} 
+      style={{backgroundColor: '#7950f2', color: '#fff'}} 
       onClick={onClick}
     >
       {children}
